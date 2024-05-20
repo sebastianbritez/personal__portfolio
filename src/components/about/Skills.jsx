cc9a4c414dfb0eb7ce354a26cb7f7dc5a3c135fe
sebastianbritez <sebastianbritezdevelop@gmail.com>
2024-05-20 17:22:33 -0400

    k

--- src/components/about/Skills.jsx
+++ src/components/about/Skills.jsx
@@ -4,8 +4,28 @@
 export const Skills = () => {
   return (
     <section className="skills__section grid">
-        
-        
+        <h3 className="skills__title">Desarrollo Web</h3>
+            <div className="skills__grid">
+                <div className="skills__item">React</div>
+                <div className="skills__item">React Native</div>
+                <div className="skills__item">Python</div>
+                <div className="skills__item">Django</div>
+                <div className="skills__item">Html</div>
+                <div className="skills__item">css</div>
+                <div className="skills__item">Javascript</div>
+        </div>
+        <h3 className="skills__title">Diseno Grafico</h3>
+            <div className="skills__grid">
+                <div className="skills__item">Inkscape</div>
+                <div className="skills__item">Illustrator</div>
+                <div className="skills__item">Aseprite</div>
+            </div>
+        <h3 className="skills__title">Otras Herramientas</h3>
+            <div className="skills__grid">
+                <div className="skills__item">Office</div>
+                <div className="skills__item">Notion</div>
+                <div className="skills__item">IA</div>
+            </div>
     </section>
   )
 }
